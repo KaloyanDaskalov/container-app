f0321b9944d848764e2fd9df109f161aeabfb353
Kaloyan Daskalov <kdaskalov78@gmail.com>
2021-03-10 12:18:06 +0200

    adding files and folders for form component

---
+++ src/components/UI/FormCard/FormCard.js
@@ -0,0 +1,13 @@
+import Background from '../../Background/Background';
+
+import classes from './FormCard.module.css';
+
+export default function FormCard() {
+    return (
+        <Background>
+            <div className={classes.formCard}>
+
+            </div>
+        </Background>
+    )
+}
